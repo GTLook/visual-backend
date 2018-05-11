f76c32479dc3746386c044b545f8e88a6b0114d7
Gavin Look <GTLook@gmail.com>
2018-05-11 12:57:49 -0700

    cleaned up seeds

--- db/seeds/00_clean.js
+++ db/seeds/00_clean.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise) {
 
-  const tablesToClean = ['users','eventMain','eventSub','data']
+  const tablesToClean = ['users','event_main','event_sub','data']
 
   return tablesToClean.reduce((acc, ele) => acc.then(() => knex(ele).del()), Promise.resolve())
 
